4211d88f566fcf82e7fcf3e9795e03031a595dbc
Samuel0102 <damu.comsa@gmail.com>
2022-01-10 08:57:44 -0300

    base do header finalizada

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="flex justify-between items-center bg-pink-200 p-4">
+        <img src="../assets/img/shared/logo.svg" alt="site-logo" class="w-8">
+        <nav></nav>
+        <img src="../assets/img/shared/icon-hamburger.svg" alt="menu-icon" class="w-6">
+    </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
